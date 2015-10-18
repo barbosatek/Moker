2cf903305b9499aa458b94ffe8e8de5affd089de
Homero Barbosa <barbosha@gmail.com>
2015-10-18 02:21:19 -0700

    Add initial features

---
+++ src/Moker.Tests/TestForTest.cs
@@ -0,0 +1,74 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Moker.Tests
+{
+    public interface IDependencyA
+    {
+        string Value { get; set; }
+    }
+
+    public interface IDependencyB
+    {
+        string Value { get; set; }
+    }
+
+    public class ClassWithNoDependencies
+    {
+    }
+
+    public class ClassWithSingleDependency
+    {
+        private readonly IDependencyA _dependencyA;
+
+        public ClassWithSingleDependency(IDependencyA dependencyA)
+        {
+            _dependencyA = dependencyA;
+        }
+
+        public IDependencyA GetDependecy()
+        {
+            return _dependencyA;
+        }
+    }
+
+    //[TestFixture]
+    //public class ClassWithSingleDependencyTests : TestFor<ClassWithSingleDependency>
+    //{
+    //    [Test]
+    //    public void Should_Initialize_Instance()
+    //    {
+    //        Assert.IsNotNull(Target);
+    //        Assert.IsTrue(Target.GetType() == typeof(ClassWithNoDependencies));
+    //    }
+    //}
+
+    [TestFixture]
+    public class ClassWithNoDependenciesTests : TestFor<ClassWithNoDependencies>
+    {
+        [Test]
+        public void Should_Initialize_Instance()
+        {
+            Assert.IsNotNull(Target);
+            Assert.IsTrue(Target.GetType() == typeof(ClassWithNoDependencies));
+        }
+
+        [Test]
+        public void Should_Add_Any_Mocks()
+        {
+            var dependency = The<IDependencyA>();
+            Assert.IsTrue(dependency.GetType() == typeof(Mock<IDependencyA>));
+        }
+
+        [Test]
+        public void Should_Persist_Mocks_As_Singletons()
+        {
+            var expected = "A";
+            var dependency = The<IDependencyA>();
+            dependency.Setup(x => x.Value).Returns(expected);
+
+            var anotherDependency = The<IDependencyA>();
+            Assert.AreEqual("A", anotherDependency.Object.Value);
+        }
+    }
+}
---
+++ src/Moker/TestFor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Moq;
+
+namespace Moker
+{
+    public class TestFor<T> where T : class
+    {
+        public T Target { get; private set; }
+        private readonly IDictionary<Type, IMockInstance> _mocks = new Dictionary<Type, IMockInstance>();
+
+        public TestFor()
+        {
+            if (TargetRequiresDependencies())
+            {
+            }
+            else
+            {
+                Target = (T)Activator.CreateInstance(typeof(T));
+            }
+        }
+        
+        public Mock<T2> The<T2>() where T2 : class
+        {
+            if (!_mocks.ContainsKey(typeof (T2)))
+            {
+                var instance = new MockInstance {Mock = new Mock<T2>()};
+                _mocks.Add(typeof (T2), instance);
+            }
+
+            return (Mock<T2>) _mocks[typeof (T2)].Mock;
+        }
+
+        private bool TargetRequiresDependencies()
+        {
+            var publicConstructors = typeof (T).GetConstructors(BindingFlags.Public);
+            return publicConstructors.Any(x => x.GetParameters().Length == 0);
+        }
+    }
+
+    internal interface IMockInstance
+    {
+        object Mock { get; set; }
+    }
+
+    internal class MockInstance : IMockInstance
+    {
+        public object Mock { get; set; }
+    }
+
+}
